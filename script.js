f9e71e7b62c7a46feaad6345a8639fbf0506ec7b
Dominik <rusac.dominik@gmail.com>
2020-06-14 20:56:26 +0200

    display balance, income and expense

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
-const money_minus = document.getElementById('money_minus');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
@@ -32,11 +32,34 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transactions => transactions.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts
+            .filter(item => item < 0)
+            .reduce((acc, item) => (acc += item), 0) * -1)
+            .toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
